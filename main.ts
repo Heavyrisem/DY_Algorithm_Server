01812beb728d42d13bb40f07cd77acdd37fc7363
heavyrisem <heavyrisem@gmail.com>
2021-09-09 02:05:01 +0900

    Fix errors

--- main.ts
+++ main.ts
@@ -5,14 +5,14 @@ import path from 'path';
 const DOCKER_IMAGENAME = 'heavyrisem/dyalgorithm';
 const JS_code = `
     setTimeout(() => {
-        throw new Error("오류가 발생했습니다다다다.");
         console.log("Hello World");
         console.log("JavaScript is running in Docker Container");
         console.log("This is Docker Container");
+        while(1) console.log(1);
         process.exit(0);
-    }, 1000);
+    }, 0);
 `;
-const TIMEOUT = '1s';
+const TIMEOUT = '1.5s';
 
 
 (() => {
@@ -24,11 +24,11 @@ const TIMEOUT = '1s';
 
     const filename = 'TEST.js';
     fs.writeFileSync(path.normalize(`${DOCKER_DIR}/${filename}`), JS_code, {encoding: 'utf-8'});
-    const script = `docker run --rm -v ${DOCKER_DIR}:/DY_Algorithm/${USER_TOKEN} -e TIMEOUT=${TIMEOUT} -e DIR="/${USER_TOKEN}"" -e COMMAND="node ${filename}" ${DOCKER_IMAGENAME}`;
+    const script = `docker run --rm -v ${DOCKER_DIR}:/DY_Algorithm/${USER_TOKEN} -e TIMEOUT=${TIMEOUT} -e DIR="/DY_Algorithm/${USER_TOKEN}" -e COMMAND="node ${filename}" ${DOCKER_IMAGENAME}`;
     console.log(script);
 
     try {
-        const stdout = execSync(script, {stdio: 'pipe', timeout: 15000}).toString();
+        const stdout = execSync(script, {stdio: 'pipe', timeout: 20000}).toString();
         console.log('================ STDOUT ================');
         console.log(stdout);
         console.log('============== STDOUT END ==============');
